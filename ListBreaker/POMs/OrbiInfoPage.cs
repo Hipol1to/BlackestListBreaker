539e29239e6edcdc1ad3cd252984a4f9cd83609f
Hipol1to <hipolitoprz2001@gmail.com>
2022-10-28 22:35:29 -0400

    file storage fixed

--- ListBreaker/POMs/OrbiInfoPage.cs
+++ ListBreaker/POMs/OrbiInfoPage.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,12 @@ namespace ListBreaker.POMs
 {
     public static class OrbiInfoPage
     {
-        private static string SubjectNeeded = "Programación II"; //nombre asignatura
+        private static string SubjectNeeded = "Electronica basica -grupo1-virtual - Puntos Tecnologicos"; //nombre asignatura
         private static By SubjectName => By.XPath("//*[@id=\"div-contenido\"]/div[4]/div/div[1]/div/table[1]/tbody/tr[2]/td[2]/table/tbody/tr[1]/td[2]/div");
-        public static void NavigateToGroupPage(IWebDriver driver)
+        public static Task NavigateToGroupPage(IWebDriver driver)
         {
             int minGroupNumber = 62337;
-            int maxGroupNumber = 65600;
+            int maxGroupNumber = 69600;
             for (int currentGroupNumber = minGroupNumber; currentGroupNumber < maxGroupNumber; currentGroupNumber++)
             {
                 driver.Navigate().GoToUrl($"{Confidential.GroupUrl}{currentGroupNumber}");
@@ -27,17 +28,24 @@ namespace ListBreaker.POMs
                     var subjectNameValue = subjectName.Text;
                     if (subjectNameValue.Contains(SubjectNeeded))
                     { //debes poner un breakpoint aqui para saber cuando encontro el grupo
-                        List<string> groups = new List<string>();
-                        string currentURL = driver.Url;
-                        groups.Add(currentURL);
-                        File.WriteAllLines(Confidential.FilePath, groups);
+                        string groupURL = $"{driver.Url} -> {SubjectNeeded}";
+                        if (!File.Exists(Confidential.FilePath))
+                        {
+                            String value = "/G";
+                            string[] folderPath = Confidential.FilePath.Split(value);
+                            Directory.CreateDirectory(folderPath[0]);
+                            using (FileStream fs = File.Create(Confidential.FilePath)) ;
+                        }
+                            File.AppendAllText(Confidential.FilePath,
+                   groupURL + Environment.NewLine);
                     }
                 }
                 catch (Exception)
                 { }
                 
             }
-            
+
+            return Task.CompletedTask;
         }
     }
 }
